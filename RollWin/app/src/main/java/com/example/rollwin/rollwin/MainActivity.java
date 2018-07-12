b788bbc7574b6e2a663ec6a1b614c2ff35a5ef90
tushark21 <tushargautamk21@gmail.com>
2018-07-13 01:21:11 +0530

    Roll&Win added

---
+++ RollWin/app/src/main/java/com/example/rollwin/rollwin/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rollwin.rollwin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    void play(View view){
+        Intent i = new Intent(this, PlayActivity.class);
+        startActivity(i);
+    }
+}
---
+++ RollWin/app/src/main/java/com/example/rollwin/rollwin/PlayActivity.java
@@ -0,0 +1,20 @@
+package com.example.rollwin.rollwin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+}
